3a103c086caecbd692547fb2b02aba59af05a99e
hansanakavinda <hansanakavinda7@gmail.com>
2025-10-17 11:07:16 +0530

    add gravity and jumping ability

--- player.gd
+++ player.gd
@@ -1,17 +1,26 @@
 extends CharacterBody2D
 
-@export var speed := 400
+@export var speed := 200
+@export var jump_force := -400
+@export var gravity := 900  # positive value (pulls down)
+@export var jump_count := 0
 
 func _physics_process(delta):
-	var dir = Vector2.ZERO
-	if Input.is_action_pressed("ui_right"):
-		dir.x += 1
-	if Input.is_action_pressed("ui_left"):
-		dir.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		dir.y += 1
-	if Input.is_action_pressed("ui_up"):
-		dir.y -= 1
+	# Apply gravity
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		if Input.is_action_just_pressed("ui_accept") && jump_count < 1:
+			jump_count += 1
+			velocity.y = jump_force
+	else:
+		# If on floor, allow jumping
+		jump_count = 0
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = jump_force
 
-	velocity = dir.normalized() * speed
+	# Horizontal movement (optional)
+	var direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * speed
+
 	move_and_slide()

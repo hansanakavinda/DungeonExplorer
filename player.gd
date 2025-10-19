23b479f1c69b04b477aba538f27aa15d1576a59f
hansanakavinda <hansanakavinda7@gmail.com>
2025-10-20 00:30:19 +0530

    player animation added

--- player.gd
+++ player.gd
@@ -1,26 +1,35 @@
 extends CharacterBody2D
 
+@onready var anim = $AnimatedSprite2D
+
 @export var speed := 200
 @export var jump_force := -400
-@export var gravity := 900  # positive value (pulls down)
-@export var jump_count := 0
+@export var gravity := 900
 
 func _physics_process(delta):
 	# Apply gravity
-	
 	if not is_on_floor():
 		velocity.y += gravity * delta
-		if Input.is_action_just_pressed("ui_accept") && jump_count < 1:
-			jump_count += 1
-			velocity.y = jump_force
 	else:
-		# If on floor, allow jumping
-		jump_count = 0
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.y = jump_force
 
-	# Horizontal movement (optional)
+	# Horizontal movement
 	var direction = Input.get_axis("ui_left", "ui_right")
 	velocity.x = direction * speed
+
+	# Flip sprite
+	if direction < 0:
+		anim.flip_h = true
+	elif direction > 0:
+		anim.flip_h = false
+
+	# Animation states
+	if not is_on_floor():
+		anim.play("jump")
+	elif direction != 0:
+		anim.play("run")
+	else:
+		anim.play("idle")
 
 	move_and_slide()
